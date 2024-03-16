f961c650abe348197fc21c1ff368ed0c5c4507a8
cameron <cmrnwlsh@gmail.com>
2024-03-16 14:32:52 -0400

    first commit

---
+++ target/debug/cli_game.d
@@ -0,0 +1,1 @@
+/home/cameron/Projects/Rust/cli_game/target/debug/cli_game: /home/cameron/Projects/Rust/cli_game/src/main.rs
---
+++ target/debug/deps/cli_game-dc61d1202cfe57b0.d
@@ -0,0 +1,5 @@
+/home/cameron/Projects/Rust/cli_game/target/debug/deps/cli_game-dc61d1202cfe57b0: src/main.rs
+
+/home/cameron/Projects/Rust/cli_game/target/debug/deps/cli_game-dc61d1202cfe57b0.d: src/main.rs
+
+src/main.rs:
